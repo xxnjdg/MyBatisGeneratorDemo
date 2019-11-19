714b7286cced202964d2cab55f000dd013c7d8d0
xxnjdg <1422570468@qq.com>
2019-11-20 03:55:28 +0800

    first commit

---
+++ src/main/java/io/xxnjdg/mybaitsgenerator/MyGenerator.java
@@ -0,0 +1,31 @@
+package io.xxnjdg.mybaitsgenerator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyGenerator {
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+//        File configFile = new File("generatorConfig.xml");
+        InputStream configFile = MyGenerator.class.getResourceAsStream("/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+    }
+}
